531ea894d82fb4dba8b4039fd8172f4feeb577d1
RovshenAshirov <rovshenashirov1619@gmail.com>
2025-03-06 11:25:50 +0500

    Variables

---
+++ 2-types/01-Variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 30;
+//        int age = 30, temperature = 20;
+        int temperature = 20;
+        System.out.println(age);
+        age = 35;
+        System.out.println(age);
+
+        int herAge = age;
+        System.out.println(herAge);
+    }
+}
